5035da71a66057f67370ddd9e475ee086992c28b
Efe Can Genç <efe.ecg@gmail.com>
2024-12-07 01:45:17 +0300

    Add files via upload

---
+++ Flutter_Project_Dunya_Gundemi/lib/main.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Dünya Gündemi',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dünya Gündemi'),
+      ),
+      body: const Center(
+        child: Text('Haberler burada görüntülenecek.'),
+      ),
+    );
+  }
+}
